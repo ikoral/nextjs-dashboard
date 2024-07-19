88f25511a21042143acae38e67d094a5d9badaa7
ikoral <ilhankoral@outlook.com>
2024-07-19 23:43:10 +0200

    resolve conflicts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,7 @@
-import { Inter, Lusitana } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
-export const lusitana = Lusitana({
-  weight: ["400", "700"],
+export const poppins = Poppins({
   subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
 });
